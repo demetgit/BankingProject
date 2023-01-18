99e90baf44c20ab588124349aa931d945868f7f9
demetgit <demetkuzikk@gmail.com>
2023-01-18 19:35:39 +0300

    c

--- force-app/main/default/lwc/firstnameOnchange/firstnameOnchange.js
+++ force-app/main/default/lwc/firstnameOnchange/firstnameOnchange.js
@@ -2,10 +2,18 @@ import { LightningElement } from 'lwc';
 
 export default class FirstnameOnchange extends LightningElement {
 
-firstname;
-
+firstname='';
+lastname='';
 isimHandler(event){
        this.firstname   =event.target.value;
+}
+lastname;
+soyadHandler(event){
+this.lastname=event.target.value;
+}
+
+get fullName(){
+    return `${this.firstname} ${this.lastname}`;
 }
 
 
