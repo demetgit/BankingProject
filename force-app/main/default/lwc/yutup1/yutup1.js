2aba03dc13e43d0e1898ebb9a3ce802f31fddb07
demetgit <demetkuzikk@gmail.com>
2023-01-18 18:50:44 +0300

    v2

---
+++ force-app/main/default/lwc/yutup1/yutup1.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class Yutup1 extends LightningElement {
+
+   // name="Demet";
+
+   fname='Demet';
+   lname='kuzk';
+   phone=12345;
+   email='dem@k.com'
+
+}
